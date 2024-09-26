35957696c4e832c8e7d102dea20b884789594cf5
A718\1dam <polgorrindo@gmail.com>
2024-09-26 20:57:12 +0200

    no message

---
+++ Ejercicios/src/main/java/org/example/BocadilloEj5.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class BocadilloEj5 {
+    public void ejecutar(){
+        System.out.println("Tienes tomate?");
+        Scanner entrada = new Scanner(System.in);
+        String valor = entrada.next();
+        System.out.println(valor);
+    }
+}
--- Ejercicios/src/main/java/org/example/Main.java
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       Ejercicio5 ejercicio5 = new Ejercicio5();
-       ejercicio5.ejecutar();
+       BocadilloEj5 bocadilloEj5 = new BocadilloEj5();
+       bocadilloEj5.ejecutar();
         //operadoresEjemplo.operadores();
         //operadoresEjemplo.asignacion();
         //operadoresEjemplo.comparacion();
