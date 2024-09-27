0bb3198105ab9e6c89062f766292831329337846
DESKTOP-40LQRNB\polgo <polgorrindo@gmail.com>
2024-09-27 10:56:36 +0200

    Ejercicio5bocata bien hecho

--- Ejercicios/src/main/java/org/example/BocadilloEj5.java
+++ Ejercicios/src/main/java/org/example/BocadilloEj5.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class BocadilloEj5 {
     public void ejecutar(){
+        System.out.println("Responda a las siguientes questiones con un sí o un no");
         System.out.println("Tienes tomate?");
         Scanner entrada = new Scanner(System.in);
         String valor = entrada.next();
-        System.out.println(valor);
+
+        System.out.println("Tienes aceite?");
+        String valor1 = entrada.next();
+
+        System.out.println("Tienes jamón?");
+        String valor2 = entrada.next();
+
+        if(valor.equals("no") || valor1.equals("no") || valor2.equals("no")){
+            System.out.println("Vamos de compras");
+        }else{
+            System.out.println("Vamos a hacer el bocadillo");
+        }
     }
 }
