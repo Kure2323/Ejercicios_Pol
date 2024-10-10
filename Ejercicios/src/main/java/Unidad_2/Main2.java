aa8a94974e40c8c5e9cebad1d4a00f5fb799fbc5
A718\1dam <polgorrindo@gmail.com>
2024-10-10 20:44:39 +0200

    no message

--- Ejercicios/src/main/java/Unidad_2/Main2.java
+++ Ejercicios/src/main/java/Unidad_2/Main2.java
@@ -3,7 +3,7 @@ package Unidad_2;
 public class Main2 {
     public static void main(String[] args) {
 
-        Ejercicio5 ejercicio5 = new Ejercicio5();
-        ejercicio5.ejecutar();
+        Proyecto_Num_Suerte proyectoNumSuerte = new Proyecto_Num_Suerte();
+        proyectoNumSuerte.ejecutar();
     }
 }
--- Ejercicios/src/main/java/Unidad_2/Proyecto_Num_Suerte.java
+++ Ejercicios/src/main/java/Unidad_2/Proyecto_Num_Suerte.java
@@ -1,4 +1,27 @@
 package Unidad_2;
 
+import java.util.Scanner;
+
 public class Proyecto_Num_Suerte {
+    public void ejecutar(){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce tu fecha de nacimiento: (dd/mm/aaaa)");
+        String entry = entrada.nextLine();
+
+        String fecha = entry.replace("/","");
+        String dd = fecha.substring(0,2);
+        int dia = Integer.parseInt(dd);
+        String mm = fecha.substring(2,4);
+        int mes = Integer.parseInt(mm);
+        String aa = fecha.substring(4);
+        int año = Integer.parseInt(aa);
+
+        int dma = dia + mes + año; //Operación
+
+        
+
+        System.out.println(dia + " " + mes + " " + año);
+
+    }
 }
