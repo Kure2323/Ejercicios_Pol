661e4bb66726890d5253c0692a041f77ba2dfa8b
DESKTOP-R22UDLR\Pol <polgorrindo@gmail.com>
2024-11-09 18:29:31 +0100

    Terminado (Faltan comentarios)

--- Ejercicios/src/main/java/Unidad_3/SamuráisP1.java
+++ Ejercicios/src/main/java/Unidad_3/SamuráisP1.java
@@ -1,6 +1,7 @@
 package Unidad_3;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,14 +14,13 @@ public class SamuráisP1 {
 
         int suma1 = 0;
         boolean ver = false;
-        String[] team1;
+        String[] team1 = new String[0];
         int[] equipo1 = new int[7];
 
-        String[] team2;
+        String[] team2 = new String[7];
         int[] equipo2 = new int[7];
         int suma2 = 0;
         boolean vor = false;
-
 
         //8888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
@@ -29,27 +29,32 @@ public class SamuráisP1 {
         do {
             suma1 = 0;
             System.out.println("Introduce la potencia de los samurais:");
-            String entry1 = entrada.nextLine();
-            team1 = entry1.replace(" ", "").split(",");
-            System.out.println(Arrays.toString(team1));
-            if (team1.length != 7) {
-                System.out.println("El equipo debe de ser de 7");
-            } else {
-                for (int i = 0; i < 7; i++) {
-                    int num = Integer.parseInt(team1[i]);
-                    suma1 += num;
-                    equipo1[i] = num;
-                    if (num > 24 || num < 1) {
-                        ver = true;
+            try{
+                String entry1 = entrada.nextLine();
+                team1 = entry1.replace(" ", "").split(",");
+                System.out.println(Arrays.toString(team1));
+                if (team1.length != 7) {
+                    System.out.println("El equipo debe de ser de 7");
+                } else {
+                    for (int i = 0; i < 7; i++) {
+                        int num = Integer.parseInt(team1[i]);
+                        suma1 += num;
+                        equipo1[i] = num;
+                        if (num > 24 || num < 1) {
+                            ver = true;
+                        }
+                    }
+                    if (ver == true) {
+                        System.out.println("Los valores no deben ser mayores a 24 o menores a 1.");
+                    }
+                    if (suma1 != 30) {
+                        System.out.println("ERROR. La potencia total no suma 30.");
                     }
                 }
-                if (ver == true) {
-                    System.out.println("Los valores no deben ser mayores a 24 o menores a 1.");
-                }
-                if (suma1 != 30) {
-                    System.out.println("ERROR. La potencia total no suma 30.");
-                }
+            }catch (NumberFormatException e){
+                System.out.println("Debe introducir únicamente números y entre comas de la siguiente manera: 1,2,3,4,5,6,7");
             }
+
         } while (team1.length != 7 || suma1!= 30 || ver == true);
         System.out.println("Equipo completado.");
 
@@ -60,27 +65,32 @@ public class SamuráisP1 {
         do {
             suma2 = 0;
             System.out.println("Introduce la potencia de los samurais:");
-            String entry2 = entrada.nextLine();
-            team2 = entry2.replace(" ","").split(",");
-            System.out.println(Arrays.toString(team2));
-            if (team2.length != 7) {
-                System.out.println("El equipo debe de ser de 7");
-            } else {
-                for (int i = 0; i < 7; i++) {
-                    int num2 = Integer.parseInt(team2[i]);
-                    suma2 += num2;
-                    equipo2[i] = num2;
-                    if (num2 > 24 || num2 < 1) {
-                        vor = true;
+            try{
+                String entry2 = entrada.nextLine();
+                team2 = entry2.replace(" ","").split(",");
+                System.out.println(Arrays.toString(team2));
+                if (team2.length != 7) {
+                    System.out.println("El equipo debe de ser de 7");
+                } else {
+                    for (int i = 0; i < 7; i++) {
+                        int num2 = Integer.parseInt(team2[i]);
+                        suma2 += num2;
+                        equipo2[i] = num2;
+                        if (num2 > 24 || num2 < 1) {
+                            vor = true;
+                        }
+                    }
+                    if (vor == true) {
+                        System.out.println("Los valores no deben ser mayores a 24 o menores a 1.");
+                    }
+                    if (suma2 != 30) {
+                        System.out.println("ERROR. La potencia total no suma 30.");
                     }
                 }
-                if (vor == true) {
-                    System.out.println("Los valores no deben ser mayores a 24 o menores a 1.");
-                }
-                if (suma2 != 30) {
-                    System.out.println("ERROR. La potencia total no suma 30.");
-                }
+            }catch (NumberFormatException e){
+                System.out.println("Debe introducir únicamente números y entre comas de la siguiente manera: 1,2,3,4,5,6,7");
             }
+            
         } while (team2.length != 7 || suma2!= 30 || vor == true);
         System.out.println("Equipo completado.");
 
