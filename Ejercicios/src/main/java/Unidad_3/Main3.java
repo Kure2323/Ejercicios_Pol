2052ec89dd301d215855857e1c7fad468a9d2f45
A718\1dam <polgorrindo@gmail.com>
2024-11-22 16:32:06 +0100

    Ejercicios de matrices; ganas de práctica

--- Ejercicios/src/main/java/Unidad_3/Main3.java
+++ Ejercicios/src/main/java/Unidad_3/Main3.java
@@ -3,6 +3,6 @@ package Unidad_3;
 public class Main3 {
     public static void main(String[] args){
         Matrices matrices = new Matrices();
-        matrices.ejsmatriz2();
+        matrices.vectores_matrices();
     }
 }
--- Ejercicios/src/main/java/Unidad_3/Matrices.java
+++ Ejercicios/src/main/java/Unidad_3/Matrices.java
@@ -203,22 +203,159 @@ public class Matrices {
     }
     public void ejsmatriz2() {
         Random rdm = new Random();
+        int suma = 0;
+        int filas = rdm.nextInt(1,6);
+        int columnas = rdm.nextInt(1,6);
 
-        int[][] matriz = new int[rdm.nextInt(1,6)][rdm.nextInt(1,6)];
+        int matriz[][] = new int[filas][columnas];
 
-        int suma_filas = 0;
-        int suma_columna = 0;
         for (int i = 0; i < matriz.length; i++) {
-            System.out.print("Fila " + (i+1) + ":");
-            suma_columna = 0;
-            suma_filas = 0;
             for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = rdm.nextInt(1,20);
-                suma_columna = suma_columna + matriz[i][j];
+
+                matriz[i][j] = rdm.nextInt(10) + 1;
+
             }
-            System.out.println(suma_columna);
+        }
+
+        for (int[] fila: matriz) {
+            for (int columna : fila) {
+                System.out.print(columna + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                suma+=matriz[i][j];
+            }
+            System.out.println("Fila " + i + ": " + suma);
+            suma=0;
+        }
+        System.out.println();
+
+        for (int i = 0; i < matriz[0].length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                suma+=matriz[j][i];
+            }
+            System.out.println("Columna " + i + ": " + suma);
+            suma=0;
 
         }
+
+
+    }
+    public void ejsmatriz3() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce la cantidad de alumnos:");
+        int filas = entrada.nextInt();
+        System.out.println("Introduce la cantidad de asignaturas:");
+        int columnas = entrada.nextInt();
+        String[][] matriz = new String[filas + 1][columnas + 1];
+        int pos = 1;
+
+        for (int i = 1; i < matriz.length; i++) {   //PIDO EL NOMBRE DE LOS ESTUDIANTES CON UN BUCLES FOR POR POSICIÓNES
+            System.out.println("Introduce el nombre del estudiante número " + i);
+            matriz[i][0] = entrada.next();
+        }
+
+        for (int i = 1; i < matriz[0].length; i++) {    //PIDO EL NOMBRE DE LAS ASIGNATURAS
+            System.out.println("Introduce el nombre de la asignatura número " + i);
+            matriz[0][i] = entrada.next();
+        }
+
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz[0].length; j++) {
+                System.out.println("Introduce la nota de " + matriz[i][0] + " para la asignatura de " + matriz[0][j]);
+                matriz[i][j] = entrada.next();
+            }
+
+        }
+        matriz[0][0] = "Estudiantes";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //Nota media
+        float media = 0;
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz[0].length; j++) {
+                media = Integer.parseInt(matriz[i][j]) + media;
+            }
+            System.out.println("La media de " + matriz[i][0] + " es : " + (media/columnas));
+            media=0;
+        }
+        System.out.println();
+        for (int i = 1; i < matriz[0].length; i++) {
+            for (int j = 1; j < matriz.length; j++) {
+                media = Integer.parseInt(matriz[j][i]) + media;
+            }
+            System.out.println("La media de " + matriz[0][i] + " es : " + (media/filas));
+            media=0;
+        }
+    }
+    public void vectores_matrices() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int vector[] = {3,4,5,3,2};
+
+        int matriz[][] = new int[2][vector.length];
+
+        for (int i = 0; i < vector.length; i++) {
+            matriz[0][i] = vector[i];
+
+        }
+        for (int[] filas : matriz) {
+            for (int columnas : filas) {
+                System.out.print(columnas + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int matriz2[][] = new int[3][4];
+
+        for (int i = 0; i < matriz2.length; i++) {
+            System.out.println("Introduce valores de la fila " + i);
+            String fila[] = entrada.next().split(",");
+            for (int j = 0; j < matriz2[i].length; j++) {
+                matriz2[i][j] = Integer.parseInt(fila[j]);
+            }
+        }
+        for (int[] filas : matriz2) {
+            for (int columnas : filas) {
+                System.out.print(columnas + " ");
+            }
+            System.out.println();
+        }
+
+
+        String matriz3[][] = new String[10][10];
+
+        for (int i = 0; i < matriz3.length; i++) {
+            System.out.println("Introduce valores de la fila " + i);
+            String fila[] = entrada.next().split("");
+            for (int j = 0; j < matriz3[i].length; j++) {
+                matriz3[i][j] = fila[j];
+            }
+        }
+        for (String[] filas : matriz3) {
+            for (String columnas : filas) {
+                System.out.print(columnas + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
 
 
 
