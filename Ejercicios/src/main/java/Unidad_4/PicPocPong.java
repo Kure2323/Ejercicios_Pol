dd54fc54fefb3c910d89ab93acd9190cb0aef1e5
Kure2323 <polgorrindo@gmail.com>
2024-12-13 18:44:20 +0100

    Algo

---
+++ Ejercicios/src/main/java/Unidad_4/PicPocPong.java
@@ -0,0 +1,31 @@
+package Unidad_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PicPocPong {
+
+    public static void Ejercicio() {
+
+
+        Scanner entrada = new Scanner(System.in);
+
+        String datos = entrada.nextLine();
+
+
+        String[] vector = datos.split(" ");
+
+        Method.Datos(vector);
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
