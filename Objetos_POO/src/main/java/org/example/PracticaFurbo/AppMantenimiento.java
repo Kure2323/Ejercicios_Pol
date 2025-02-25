3dcebdaa2e2aab1c63b66f568eafbcfe0c0646a3
Pol <polgorrindo@gmail.com>
2025-02-25 19:43:26 +0100

    Implementación de clase AppMantenimiento

--- Objetos_POO/src/main/java/org/example/PracticaFurbo/AppMantenimiento.java
+++ Objetos_POO/src/main/java/org/example/PracticaFurbo/AppMantenimiento.java
@@ -2,6 +2,24 @@ package org.example.PracticaFurbo;
 
 public class AppMantenimiento {
 
-    
+    private void pantallaPrincipal() {
+        System.out.println("=== App de mantenimiento del MUTXAMEL FC ===");
+        System.out.println();
+        System.out.println("    [1]. Mantenimiento de jugadores");
+        System.out.println("        Dentro podremos añadir jugadores, modificar datos y añadir acompañantes (sólo seniors).");
+        System.out.println("    [2]. Mantenimiento de entrenadores (añadir-modificar-salir)");
+        System.out.println("        Dentro podremos añadir entrenadores y modificar sus datos.");
+        System.out.println("    [3]. Mantenimiento masajistas (añadir-modificar datos-salir) ");
+        System.out.println("        Dentro podremos añadir masajistas y modificar sus datos.");
+        System.out.println("    [4]. Consultar equipos");
+        System.out.println("        Dentro se deben listar los tipos de equipos del club y elegir uno.");
+        System.out.println("    [X]. Salir");
+        System.out.println();
+        System.out.println("=============================================");
+        System.out.println();
+        System.out.println("Selecciona una opción -->");
+    }
+
+
 
 }
